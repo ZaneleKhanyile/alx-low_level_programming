f343104c6efe3500c305aaf9145ebf83bb70f88e
ZaneleKhanyile <zanelekhanyile@296@gmail.com>
2023-08-16 17:14:04 -0700

    function_pointers git commit -m function_pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,15 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * print_name - print name using pointer to function
- * @name: string to add
- * @f: pointer to function
- * Return: nothing
- **/
+ * print_name - Prints a name using the provided function
+ * @name: The name to be printed
+ * @f: A function pointer to the printing function
+ *
+ * Description: This function takes a name and a function pointer as
+ * arguments. It calls the provided function to print the given name.
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
-
-
