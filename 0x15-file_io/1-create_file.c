ada65fe3469129f2afc5d97d2a1b76f781e4f680
ZaneleKhanyile <zanelekhanyile296@gmail.com>
2023-10-30 10:16:19 -0700

    file.io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,6 @@
+#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -13,28 +13,29 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file_descriptor, write_status, close_status;
-	mode_t permissions = S_IRUSR | S_IWUSR;
+	int file_descriptor, write_status;
+	ssize_t length = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, permissions);
+	file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file_descriptor == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		write_status = write(file_descriptor, text_content, strlen(text_content));
-		if (write_status == -1)
+		length = write(file_descriptor, text_content, strlen(text_content));
+		if (length == -1)
 		{
 			close(file_descriptor);
 			return (-1);
 		}
 	}
 
-	close_status = close(file_descriptor);
-	if (close_status == -1)
+	close(file_descriptor);
+
+	if (length == -1)
 		return (-1);
 
 	return (1);
